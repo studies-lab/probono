c92f1c1e8ab37d97294473eb7c5e296e3e1e386d
MarthaDs <marthadntas@gmail.com>
2020-06-02 16:58:40 -0300

    [CONNECT] connect api server with mongodb

---
+++ app/models/index.js
@@ -0,0 +1,15 @@
+const dbConfig = require("../config/db.config.js");
+
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+const db = {};
+db.mongoose = mongoose;
+db.url = dbConfig.url;
+db.lawers = require("./lawer.model.js")(mongoose);
+db.clients = require("./client.models.js")(mongoose);
+db.rules = require("./rule.models.js")(mongoose);
+db.processes = require("./process.models.js")(mongoose);
+db.movements = require("./movement.models.js")(mongoose);
+
+module.exports = db;
--- server.js
+++ server.js
@@ -12,9 +12,19 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.json({ message: "teste" });
-});
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
 const PORT = process.env.PORT || 5000;
 
