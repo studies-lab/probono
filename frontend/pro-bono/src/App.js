fc8760a2e6e7725438197ca3c35159d88baebe91
MarthaDs <marthadntas@gmail.com>
2020-06-02 19:44:54 -0300

    [FIX] fix typing mistakes

--- frontend/pro-bono/src/App.js
+++ frontend/pro-bono/src/App.js
@@ -1,9 +1,22 @@
-import React, { Componet } from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-class App extends Componet {
+import AddLawer from "./components/add-lawer.component";
+import Lawer from "./components/add-lawer.component";
+
+class App extends Component {
   render() {
-    //
+    return (
+      <Router>
+        <div className="container mt-3">
+            <Switch>
+              <Route exact path="/add" component={AddLawer} />
+              <Route path="/lawers/:id" component={Lawer} />
+            </Switch>
+          </div>
+      </Router>
+    );
   }
 }
 
